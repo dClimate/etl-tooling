c5d170947add2935aecd49bc621e93c61468de74
Chris Rossi <chris@christophermrossi.com>
2024-06-13 10:10:26 -0400

    Initial commit

---
+++ noxfile.py
@@ -0,0 +1,71 @@
+import os
+import pathlib
+import shutil
+
+import nox
+
+# numba is not supported on Python 3.12
+ALL_INTERPRETERS = (
+    "3.10",
+    "3.11",
+)
+CODE = "dc_etl"
+DEFAULT_INTERPRETER = "3.10"
+HERE = pathlib.Path(__file__).parent
+
+
+@nox.session(py=ALL_INTERPRETERS)
+def unit(session):
+    session.install("-e", ".[testing]")
+    session.run(
+        "pytest",
+        f"--cov={CODE}",
+        "--cov=tests.unit",
+        "--cov-append",
+        "--cov-config",
+        HERE / ".coveragerc",
+        "--cov-report=term-missing",
+        "tests/unit",
+    )
+
+
+@nox.session(py=DEFAULT_INTERPRETER)
+def cover(session):
+    session.install("coverage")
+    session.run("coverage", "report", "--fail-under=100", "--show-missing")
+    session.run("coverage", "erase")
+
+
+@nox.session(py=DEFAULT_INTERPRETER)
+def lint(session):
+    session.install("black", "flake8", "flake8-pyproject")
+    run_black(session, check=True)
+    session.run("flake8", CODE, "tests")
+
+
+@nox.session(py=DEFAULT_INTERPRETER)
+def blacken(session):
+    # Install all dependencies.
+    session.install("black")
+    run_black(session)
+
+
+def run_black(session, check=False):
+    args = ["black"]
+    if check:
+        args.append("--check")
+    args.extend(["noxfile.py", CODE, "tests"])
+    session.run(*args)
+
+
+# @nox.session(py=DEFAULT_INTERPRETER)
+# def doc(session):
+#     session.install("-e", ".[doc]")
+#     shutil.rmtree(os.path.join("doc", "_build"), ignore_errors=True)
+#     session.run(
+#         "sphinx-build",
+#         "-W",  # warnings as errors
+#         "-T",  # show full traceback on exception
+#         os.path.join("doc", ""),
+#         os.path.join("doc", "_build"),
+#     )
