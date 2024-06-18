a670f1a92e8f759ba17fa21e60291b6be5760b04
Md Abid Sikder <abid@abidsikder.com>
2024-06-18 16:16:33 -0400

    Add abstractmethod decorator for Extractor abstract class

--- dc_etl/extract.py
+++ dc_etl/extract.py
@@ -1,9 +1,9 @@
 import abc
+from abc import abstractmethod
 import typing
 
 import numpy
 import xarray
-
 
 class Timespan(typing.NamedTuple):
     start: numpy.datetime64
@@ -13,6 +13,7 @@ class Timespan(typing.NamedTuple):
 class Extractor(abc.ABC):
     """A component responsible for downloading data from a data source and providing it to a Transformer."""
 
+    @abstractmethod
     def get_remote_timespan(self) -> Timespan:
         """Get the timespan available remotely for this dataset.
 
@@ -25,6 +26,7 @@ class Extractor(abc.ABC):
             Timespan including the first and last timestamps available in the source data.
         """
 
+    @abstractmethod
     def get_local_timespans(self) -> list[Timespan]:
         """Get all of the timespans that have been downloaded and are available locally.
 
@@ -37,6 +39,7 @@ class Extractor(abc.ABC):
 
         """
 
+    @abstractmethod
     def download(self, span: Timespan):
         """Download any files from the remote data provider corresponding to the given timespan.
 
@@ -49,6 +52,7 @@ class Extractor(abc.ABC):
         nothing if all data for the given timespan is already downloaded.
         """
 
+    @abstractmethod
     def extract(self, span: Timespan) -> typing.Generator[xarray.Dataset, None, None]:
         """Extract any files from the remote data provider corresponding to the given timespan.
 
