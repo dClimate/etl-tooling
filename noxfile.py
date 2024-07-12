e84e04094e9d86daec930be0812fcd042e929395
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2024-07-12 16:40:19 -0400

    Simplify testing interpreters to just 3.10 and ruff

--- noxfile.py
+++ noxfile.py
@@ -1,65 +1,41 @@
-import os
-import pathlib
-import shutil
-
 import nox
 
-# numba is not supported on Python 3.12
-ALL_INTERPRETERS = (
-    "3.10",
-    "3.11",
-    "3.12",
-)
-CODE = "dc_etl"
-DEFAULT_INTERPRETER = "3.10"
-HERE = pathlib.Path(__file__).parent
 
-
-@nox.session(py=ALL_INTERPRETERS)
+@nox.session()
 def unit(session):
     session.install("-e", ".[testing]")
     session.run(
         "pytest",
-        f"--cov={CODE}",
+        "--cov=dc_etl",
         "--cov=tests.unit",
         "--cov-append",
         "--cov-config",
-        HERE / ".coveragerc",
+        ".coveragerc",
         "--cov-report=term-missing",
         "tests/unit",
     )
 
 
-@nox.session(py=DEFAULT_INTERPRETER)
+@nox.session()
 def cover(session):
     session.install("coverage")
     session.run("coverage", "report", "--fail-under=100", "--show-missing")
     session.run("coverage", "erase")
 
 
-@nox.session(py=DEFAULT_INTERPRETER)
+@nox.session()
 def lint(session):
-    session.install("black", "flake8", "flake8-pyproject")
-    run_black(session, check=True)
-    session.run("flake8", CODE, "tests")
+    session.install("ruff")
+    session.run("ruff", "check")
 
 
-@nox.session(py=DEFAULT_INTERPRETER)
-def blacken(session):
-    # Install all dependencies.
-    session.install("black")
-    run_black(session)
+@nox.session()
+def format(session):
+    session.install("ruff")
+    session.run("ruff", "format", "--check")
 
 
-def run_black(session, check=False):
-    args = ["black"]
-    if check:
-        args.append("--check")
-    args.extend(["noxfile.py", CODE, "tests"])
-    session.run(*args)
-
-
-@nox.session(py=DEFAULT_INTERPRETER)
+@nox.session()
 def system(session):
     # if not check_kubo():
     #     session.skip("No IPFS server running")
@@ -69,14 +45,17 @@ def system(session):
         "pytest",
         "--cov=tests.system",
         "--cov-config",
-        HERE / ".coveragerc",
+        ".coveragerc",
         "--cov-report=term-missing",
         "--cov-fail-under=100",
         "tests/system",
     )
 
 
-# @nox.session(py=DEFAULT_INTERPRETER)
+# Reimport os and shutil for doc test in the future if needed
+# import os
+# import shutil
+# @nox.session()
 # def doc(session):
 #     session.install("-e", ".[doc]")
 #     shutil.rmtree(os.path.join("doc", "_build"), ignore_errors=True)
